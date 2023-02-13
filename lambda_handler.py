dbdbd28dacc8fae27856b3141eac1a955caee428
Joshtna <124188811+Joshtna@users.noreply.github.com>
2023-02-13 14:44:49 +0530

    Update lambda_handler.py

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,9 +1,8 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
+ 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Good Morning!')
     }
-[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cs(Morning[C[C[Cg[C[C[C[[C[C[C[C[C!')[B[B[B
