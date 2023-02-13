ca3f178b49be90fb1b4efa4ac4613bbede2ddf6f
LAKSHMI, MUSINI JOSHTNA <musinijoshtna.lakshmi@nissanmotor.com>
2023-02-13 14:36:26 +0530

    first commit

---
+++ lambda_handler.py
@@ -0,0 +1,9 @@
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cs(Morning[C[C[Cg[C[C[C[[C[C[C[C[C!')[B[B[B
